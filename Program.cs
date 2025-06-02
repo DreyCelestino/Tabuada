dac69a3f94588c4e900f52b5a9eb0cc6dae36d87
DreyCelestino <andreycelestino05@gmail.com>
2025-06-02 08:48:09 -0300

    v1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+string lerInput;
+int input;
+
+int[] output = new int[11];
+
+
+
+Console.WriteLine("Digite um número");
+lerInput = Console.ReadLine()!;
+
+while (!(Int32.TryParse(lerInput, out int teste)))
+{
+    Console.WriteLine("Não é um número! Tente novamente.");
+    lerInput = Console.ReadLine()!;
+}
+
+input = Convert.ToInt32(lerInput);
+
+for (int i = 0; i < 11; i++)
+{
+    output[i] = input * i;
+    Console.WriteLine($"{input} * {i} = {output[i]}");
+
+}
